2b07cd65ee7f2a9892c391d021727c8fd93ac5f2
BeatrizReisDutra <beatrizreisdutra@gmail.com>
2025-10-23 20:07:07 -0400

    html

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 meu_app = Flask(__name__)
 @meu_app.route('/')
 def inicio():
-    return 'Olá, turma!. Sou o Flask'
+    return render_template('base.html')
 
 if __name__ == "__main__":
     meu_app.run(port= 8000)
